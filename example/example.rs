f1f8384371f1cd6ec426fa423e8cab8606414d35
zaiic <zaiic@qq.com>
2022-08-21 21:52:45 +0800

    feat: Added a test item into the example.

--- example/example.rs
+++ example/example.rs
@@ -3,7 +3,7 @@ use t1gars::*;
 fn test_cbw8() -> Result<(), Error> {
     let tga = Tga::new("example/images/CBW8.TGA")?;
     assert_eq!(tga.header.get_pixel_format().unwrap(), TgaPixelFormat::BW8);
-    tga.save("example/images/temp_CBBW8.tga")?;
+    tga.save("example/images/temp_cbw8.tga")?;
 
     Ok(())
 }
@@ -16,7 +16,16 @@ fn test_ctc24() -> Result<(), Error> {
     Ok(())
 }
 
+fn test_utc24() -> Result<(), Error> {
+    let tga = Tga::new("example/images/UTC24.TGA")?;
+    assert_eq!(tga.header.get_pixel_format().unwrap(), TgaPixelFormat::RGB24);
+    tga.save("example/images/temp_utc24.tga")?;
+
+    Ok(())
+}
+
 fn main() {
     println!("{:?}",test_cbw8());
     println!("{:?}",test_ctc24());
+    println!("{:?}",test_utc24());
 }
